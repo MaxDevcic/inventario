bd494bcd661762f52084ee27ab5d1b6670439eaf
MaxDevcic <ma.devcic@duocuc.cl>
2025-06-18 13:31:01 -0400

    test findAll

--- src/test/java/com/MaxdInv/inventario/service/InventarioServiceTest.java
+++ src/test/java/com/MaxdInv/inventario/service/InventarioServiceTest.java
@@ -8,10 +8,10 @@ import org.mockito.MockitoAnnotations;
 
 import com.MaxdInv.inventario.model.Inventario;
 import com.MaxdInv.inventario.repository.InventarioRepository;
-/* 
+ 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;*/
+//import java.util.Optional;
 
 /*import static org.mockito.ArgumentMatchers.any;*/
 import static org.mockito.Mockito.*;
@@ -30,7 +30,7 @@ public class InventarioServiceTest {
     }
 
     @Test
-    void testfindAllByIdAndCodigoproducto(){
+    void testFindAllByIdAndCodigoproducto(){
         Inventario inv = new Inventario(1L, 1L, "Gomitas", 10);
         when(inventarioRepository.findByIdAndCodigoproducto(1L,1L)).thenReturn(inv);
 
@@ -41,4 +41,17 @@ public class InventarioServiceTest {
         assertThat(resultado.getCantidad()).isEqualTo(inv.getCantidad());
         verify(inventarioRepository).findByIdAndCodigoproducto(1L, 1L);
     }
+
+    @Test
+    void testFindAll(){
+        Inventario inv1 = new Inventario(1L, 1L, "Gomitas", 10);
+        Inventario inv2 = new Inventario(1L, 2L, "Coca-cola", 7);
+        Inventario inv3 = new Inventario(3L, 1L, "Gomitas", 5);
+        when(inventarioRepository.findAll()).thenReturn(Arrays.asList(inv1, inv2, inv3));
+
+        List<Inventario> resultado = inventarioService.findAll();
+        assertThat(resultado).hasSize(3).contains(inv1, inv2, inv3);
+        verify(inventarioRepository).findAll();
+    }
+
 }
