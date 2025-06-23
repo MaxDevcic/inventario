eaf9ea5ea165161271a9d1368ce81704c1bff314
MaxDevcic <ma.devcic@duocuc.cl>
2025-06-23 10:51:08 -0400

    Pruebas unitarias Service listos

--- src/test/java/com/MaxdInv/inventario/service/InventarioServiceTest.java
+++ src/test/java/com/MaxdInv/inventario/service/InventarioServiceTest.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 //import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 /*import static org.mockito.ArgumentMatchers.any;*/
 import static org.mockito.Mockito.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -54,4 +55,41 @@ public class InventarioServiceTest {
         verify(inventarioRepository).findAll();
     }
 
+    @Test
+    void testSave(){
+        Inventario inventario = new Inventario(1L,1L,"Sprite",15);
+        Inventario invsave = new Inventario(1L,1L,"Sprite",15);
+        when(inventarioRepository.save(inventario)).thenReturn(invsave);
+
+        Inventario resultado = inventarioService.save(inventario);
+        assertThat(resultado).isEqualTo(invsave);
+        verify(inventarioRepository).save(inventario);
+    }
+
+    @Test
+    void testFindAllById(){
+        Inventario i1 = new Inventario(1L,1L,"Sprite",15);
+        Inventario i3 = new Inventario(1L,2L,"CocaCola",20);
+        when(inventarioRepository.findAllById(1L)).thenReturn(Arrays.asList(i1,i3));
+
+        List<Inventario> resultado = inventarioService.findAllById(1L);
+        assertThat(resultado).hasSize(2).contains(i1,i3);
+        verify(inventarioRepository).findAllById(1L);
+    }
+
+    @Test
+    void testNuevaCant(){
+        Inventario inventarioViejo = new Inventario(1L,1L,"Sprite",15);
+        int cantidad = 1;
+        Inventario inventarioNuevo = new Inventario(1L,1L,"Sprite",16);
+        when(inventarioRepository.findByIdAndCodigoproducto(1L, 1L)).thenReturn(inventarioNuevo);
+        when(inventarioRepository.save(any(Inventario.class))).thenAnswer(i -> i.getArgument(0));
+
+        Inventario resultado = inventarioService.nuevaCant(inventarioViejo, cantidad);
+        assertThat(resultado.getId()).isEqualTo(1L);
+        assertThat(resultado.getCodigoproducto()).isEqualTo(1L);
+        assertThat(resultado.getNombreproducto()).isEqualTo("Sprite");
+        assertThat(resultado.getCantidad()).isEqualTo(16);
+        verify(inventarioRepository).save(inventarioNuevo);
+    }
 }
